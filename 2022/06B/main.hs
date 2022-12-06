73887a2060b2d22357afdb638d69944c081764af
Andrew "Gozz" Gozzard <gozzardam@gmail.com>
2022-12-06 13:16:54 +0800

    2022 Day 06

---
+++ 2022/06B/main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Data.List (findIndex, nub, tails)
+import Data.Maybe (fromJust)
+
+type Case = String
+
+type Soln = Int
+
+main :: IO ()
+main = interact process
+
+process :: String -> String
+process = showSolns . map solve . readCases
+
+readCases :: String -> [Case]
+readCases = lines
+
+showSolns :: [Soln] -> String
+showSolns = unlines . map show
+
+solve :: Case -> Soln
+solve = (+ header_length) . fromJust . findIndex ((== header_length) . length . nub) . map (take header_length) . tails
+  where
+    header_length = 14
