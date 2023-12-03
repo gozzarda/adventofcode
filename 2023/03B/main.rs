8e6650621d2935121c28782831560d2bf2d1ee74
Andrew "Gozz" Gozzard <gozzardam@gmail.com>
2023-12-03 20:35:43 +0800

    2023 Day 03 Rust

---
+++ 2023/03B/main.rs
@@ -0,0 +1,53 @@
+use std::cmp::{max, min};
+use std::io;
+
+fn main() {
+    let stdin = io::stdin();
+    let grid = stdin
+        .lines()
+        .map(|l| l.unwrap().chars().collect::<Vec<char>>())
+        .collect::<Vec<_>>();
+    let mut near: Vec<Vec<Vec<u32>>> = grid
+        .iter()
+        .map(|row| row.iter().map(|_| Vec::default()).collect())
+        .collect();
+    for row in 0..grid.len() {
+        let row_lwr = max(1, row) - 1;
+        let row_upr = min(row + 2, grid.len());
+        for col in 0..grid[row].len() {
+            if col > 0 && grid[row][col - 1].is_digit(10) {
+                continue;
+            }
+            let len = grid[row][col..]
+                .iter()
+                .take_while(|c| c.is_digit(10))
+                .count();
+            if len == 0 {
+                continue;
+            }
+            let val = grid[row][col..col + len]
+                .iter()
+                .collect::<String>()
+                .parse::<u32>()
+                .unwrap();
+            let col_lwr = max(1, col) - 1;
+            let col_upr = min(col + len + 1, grid[row].len());
+            for r in row_lwr..row_upr {
+                for c in col_lwr..col_upr {
+                    if grid[r][c] != '.' && grid[r][c].is_ascii_punctuation() {
+                        near[r][c].push(val);
+                    }
+                }
+            }
+        }
+    }
+    let mut total: u32 = 0;
+    for row in 0..grid.len() {
+        for col in 0..grid[row].len() {
+            if grid[row][col] == '*' && near[row][col].len() == 2 {
+                total += near[row][col].iter().product::<u32>();
+            }
+        }
+    }
+    println!("{}", total);
+}
