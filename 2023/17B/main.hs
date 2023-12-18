87e997a2215df0630709aa4cd129dccbfab2e2f1
Andrew "Gozz" Gozzard <gozzardam@gmail.com>
2023-12-18 18:22:14 +0800

    2023 Day 17: Refactor

--- 2023/17B/main.hs
+++ 2023/17B/main.hs
@@ -1,9 +1,9 @@
 module Main where
 
-import Data.List (uncons)
+import Data.List (scanl1, uncons)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe (catMaybes, fromJust)
+import Data.Maybe (fromJust)
 import Data.Tuple (swap)
 
 type Prob = [[Int]]
@@ -35,8 +35,8 @@ type Coord = (Int, Int)
 addCoord :: Coord -> Coord -> Coord
 addCoord (lr, lc) (rr, rc) = (lr + rr, lc + rc)
 
--- (pos, dir, steps until forced turn)
-type Vert = (Coord, Coord, Int)
+-- (pos, True iff next move must be vertical)
+type Vert = (Coord, Bool)
 
 type Weight = Int
 
@@ -46,28 +46,34 @@ minSteps = 4
 maxSteps = 10
 
 neighbours :: Map Coord Weight -> Vert -> [(Vert, Weight)]
-neighbours iwm (pos, dir@(dr, dc), steps) = catMaybes $ map (\v -> fmap ((,) v) $ weight v) $ catMaybes $ map next dirs
+neighbours iwm (pos, vertical) = concatMap dirvws dirs
   where
-    dirs = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-    next d = case d of
-      d | dir == (0, 0) -> Just (addCoord pos d, d, 1)
-      d | d == dir -> if steps < maxSteps then Just (addCoord pos d, d, steps + 1) else Nothing
-      (dr', dc') | dr' == (-dr) && dc' == (-dc) -> Nothing
-      d -> if steps >= minSteps then Just (addCoord pos d, d, 1) else Nothing
-    weight (pos, _, _) = Map.lookup pos iwm
+    dirvws dir = drop (minSteps - 1) $ take maxSteps $ zip vs ws
+      where
+        ps = tail $ iterate (addCoord dir) pos
+        vs = map (\p -> (p, not vertical)) ps
+        ws = scanl1 (+) $ takeWhileJust $ map (flip Map.lookup iwm) ps
+    dirs = if vertical then [(-1, 0), (1, 0)] else [(0, -1), (0, 1)]
+
+takeWhileJust :: [Maybe a] -> [a]
+takeWhileJust (Just x:mxs) = x : takeWhileJust mxs
+takeWhileJust _ = []
 
 dijkstras :: Map Coord Weight -> Coord -> Coord -> Maybe Weight
-dijkstras iwm src dst = go (Map.empty) (pqSingleton 0 (src, (0, 0), 0))
+dijkstras iwm src dst = go (Map.empty) $ pqFromList [(0, (src, True)), (0, (src, False))]
   where
     go vdm dvpq = case pqPop dvpq of
       Nothing -> Nothing
-      Just ((d, (p, _, _)), _) | p == dst -> Just d
+      Just ((d, (p, _)), _) | p == dst -> Just d
       Just ((_, v), dvpq') | Map.member v vdm -> go vdm dvpq'
       Just ((d, v), dvpq') -> go (Map.insertWith min v d vdm) (pqPushes (map swap ns) dvpq')
         where
           ns = map (fmap (+ d)) $ filter (\(v, _) -> Map.notMember v vdm) $ neighbours iwm v
 
 type PQueue k v = Map k [v]
+
+pqEmpty :: Ord k => PQueue k v
+pqEmpty = Map.empty
 
 pqSingleton :: Ord k => k -> v -> PQueue k v
 pqSingleton k v = Map.singleton k [v]
@@ -78,10 +84,13 @@ pqPush k v = Map.insertWith (++) k [v]
 pqPushes :: Ord k => [(k, v)] -> PQueue k v -> PQueue k v
 pqPushes kvs q = foldr (uncurry pqPush) q kvs
 
-pqPop :: Ord k => PQueue k v -> Maybe ((k, v), PQueue k v)
+pqPop :: (Show k, Show v, Ord k) => PQueue k v -> Maybe ((k, v), PQueue k v)
 pqPop q = case Map.lookupMin q of
   Nothing -> Nothing
   Just (k, []) -> pqPop q'
   Just (k, (v : _)) -> Just ((k, v), q')
   where
     q' = Map.updateMin (fmap snd . uncons) q
+
+pqFromList :: Ord k => [(k, v)] -> PQueue k v
+pqFromList = flip pqPushes pqEmpty
