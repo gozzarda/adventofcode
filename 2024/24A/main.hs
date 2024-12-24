931d53d5c96d948eb9358ac42197ecf07ad65ad7
Andrew "Gozz" Gozzard <gozzardam@gmail.com>
2024-12-24 15:08:32 +0800

    2024 Day 24

---
+++ 2024/24A/main.hs
@@ -0,0 +1,95 @@
+module Main where
+
+import Data.Bits (shift)
+import Data.Char (isDigit, isSpace)
+import Data.Functor (($>))
+import Data.List (stripPrefix)
+import Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as Map
+import Data.Maybe
+import Text.ParserCombinators.ReadP
+import qualified Text.ParserCombinators.ReadPrec as ReadPrec
+import Text.Read (readPrec)
+
+data Expr
+  = Atom Bool
+  | And String String
+  | Or String String
+  | Xor String String
+  deriving (Ord, Eq, Show)
+
+type Prob = [(String, Expr)]
+
+type Soln = Int
+
+main :: IO ()
+main = interact process
+
+process :: String -> String
+process = showSoln . solve . readProb
+
+showSoln :: Soln -> String
+showSoln = unlines . return . show
+
+readProb :: String -> Prob
+readProb = doReadP' $ parseProb <* eof
+
+doReadP' :: ReadP a -> String -> a
+doReadP' p = fromMaybe (error "Parsing failed") . doReadP p
+
+doReadP :: ReadP a -> String -> Maybe a
+doReadP p = fmap fst . listToMaybe . readP_to_S p
+
+parse :: (Read a) => ReadP a
+parse = ReadPrec.readPrec_to_P readPrec ReadPrec.minPrec
+
+parseBool :: ReadP Bool
+parseBool = (char '0' $> False) +++ (char '1' $> True)
+
+parseAtom :: ReadP (String, Expr)
+parseAtom = do
+  name <- munch (/= ':')
+  char ':'
+  skipSpaces
+  val <- parseBool
+  return (name, Atom val)
+
+parseOp :: ReadP (String -> String -> Expr)
+parseOp =
+  choice
+    [ string "AND" $> And,
+      string "OR" $> Or,
+      string "XOR" $> Xor
+    ]
+
+parseGate :: ReadP (String, Expr)
+parseGate = do
+  lname <- munch (not . isSpace)
+  skipSpaces
+  op <- parseOp
+  skipSpaces
+  rname <- munch (not . isSpace)
+  skipSpaces
+  string "->"
+  skipSpaces
+  name <- munch (not . isSpace)
+  return (name, op lname rname)
+
+parseExpr :: ReadP (String, Expr)
+parseExpr = parseAtom +++ parseGate
+
+parseProb :: ReadP Prob
+parseProb = endBy parseExpr skipSpaces
+
+solve :: Prob -> Soln
+solve kxs = sum $ map (shift 1 . fst) $ filter snd ivs
+  where
+    kxm = Map.fromList kxs
+    kvm = Map.map eval kxm
+    eval expr = case expr of
+      Atom b -> b
+      And lhs rhs -> (kvm Map.! lhs) && (kvm Map.! rhs)
+      Or lhs rhs -> (kvm Map.! lhs) || (kvm Map.! rhs)
+      Xor lhs rhs -> (kvm Map.! lhs) /= (kvm Map.! rhs)
+    kvs = Map.assocs kvm
+    ivs = mapMaybe (\(k, v) -> (,v) . read <$> stripPrefix "z" k) kvs
---
+++ 2024/24B/main.md
@@ -0,0 +1,8 @@
+# AoC 2024 Day 24B Manual Solution
+- Converted the input to graphviz DOT
+- Inspected manually to determine structure
+  - It's a ripple-carry adder with nothing fancy
+- Modify part A code to output error bits to help find issues
+- Manually inspect graph to find, highlight, and fix issues
+  - Issues often cause the layout of the graph to mess up
+- Find all four swapped pairs, sort for answer
